74d747fe377af76aac5e7dc558b218b36267b38f
kiang <kiange@gmail.com>
2013-02-08 23:08:34 +0800

    merge english strings for being able to translate it manually

--- modules/blocklanguages/translations/tw.php
+++ modules/blocklanguages/translations/tw.php
@@ -2,6 +2,5 @@
 
 global $_MODULE;
 $_MODULE = array();
-
-$_MODULE['<{blocklanguages}prestashop>blocklanguages_2575f2a453fd18a4e74e430a80eb3540'] = '添加一個語言選擇區域';
 $_MODULE['<{blocklanguages}prestashop>blocklanguages_d5988791c07fedc0e2fc77683b4e61f6'] = '語言區域';
+$_MODULE['<{blocklanguages}prestashop>blocklanguages_2575f2a453fd18a4e74e430a80eb3540'] = '添加一個語言選擇區域';
