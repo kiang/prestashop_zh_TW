a1cdaee0f7f65153e759293f042d49384c5b5a38
kiang <kiange@gmail.com>
2013-03-08 23:42:16 +0800

    english reference

---
+++ english_reference/modules/blocksearch/translations/en.php
@@ -0,0 +1,10 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blocksearch}prestashop>blocksearch-top_24c29702dc9c63dedb98e92f3028e068'] = 'Search:';
+$_MODULE['<{blocksearch}prestashop>blocksearch_31a0c03d7fbd1dc61d3b8e02760e703b'] = 'Quick Search block';
+$_MODULE['<{blocksearch}prestashop>blocksearch_99e20473c0bf3c22d7420eff03ce66c3'] = 'Adds a block with a quick search field.';
+$_MODULE['<{blocksearch}prestashop>blocksearch_24c29702dc9c63dedb98e92f3028e068'] = 'Search:';
+$_MODULE['<{blocksearch}prestashop>blocksearch_52d578d063d6101bbdae388ece037e60'] = 'Enter a product name';
+$_MODULE['<{blocksearch}prestashop>blocksearch_34d1f91fb2e514b8576fab1a75a89a6b'] = 'go';
