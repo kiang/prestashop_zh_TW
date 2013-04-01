1de4b8129f2e592fb4a4028742a711049bf8d1e6
kiang <kiange@gmail.com>
2013-04-01 11:20:29 +0800

    add translation of install

---
+++ install-dev/langs/tw/install.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+	'informations' => array(
+		'phone' => '0972-095901',
+		'documentation' => 'http://doc.prestashop.com/',
+		'documentation_upgrade' => 'http://docs.prestashop.com/display/PS15/Updating+PrestaShop',
+		'forum' => 'http://twpug.net/modules/newbb/viewforum.php?forum=43',
+		'blog' => 'http://olc.tw/',
+		'support' => 'http://olc.tw/',
+	),
+	'translations' => array(
+		'menu_welcome' => '選擇語言',
+		'menu_license' => '授權聲明',
+		'menu_system' => '系統相容性',
+		'menu_database' => '系統設定',
+		'menu_configure' => '商店資訊',
+		'menu_process' => '商店安裝',
+	),
+);
